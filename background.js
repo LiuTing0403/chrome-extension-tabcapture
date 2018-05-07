f3c0a5191daa2f696167983877a039acb7a05b55
LiuTing0403 <liuting0403@foxmail.com>
2018-05-07 18:16:23 +0800

    update

---
+++ background.js
@@ -0,0 +1,28 @@
+var MESSAGE = {
+    START_RECORDING: 'start-recording',
+    STOP_RECORDING: 'stop-recording',
+    STREAM_ID: 'stream-id',
+    USER_CANCELED: 'user-canceled'
+}
+
+var screenOptions = ['tab', 'audio']
+
+chrome.runtime.onConnect.addListener(function(port) {
+    port.onMessage.addListener(function(message) {
+        if (message.message === MESSAGE.START_RECORDING) {
+            chrome.desktopCapture.chooseDesktopMedia(screenOptions, port.sender.tab, onAccessApproved);
+        }
+    })
+
+    function onAccessApproved(chromeMediaSourceId, opts) {
+        if (!chromeMediaSourceId || !chromeMediaSourceId.toString().length) {
+            port.postMessage({message: MESSAGE.USER_CANCELED})
+            chrome.runtime.reload();
+            return;
+        }
+
+        port.postMessage({streamId: chromeMediaSourceId, opts: opts, message: MESSAGE.STREAM_ID})
+    
+    }
+})
+
---
+++ content-script.js
@@ -0,0 +1,33 @@
+var port = chrome.runtime.connect()
+
+var MESSAGE = {
+    START_RECORDING: 'start-recording',
+    STOP_RECORDING: 'stop-recording',
+    STREAM_ID: 'stream-id',
+    USER_CANCELED: 'user-canceled',
+    CHECK_STATUS: 'check-extension-ready'
+}
+
+window.onmessage = function(e) {
+    if (e.data === MESSAGE.START_RECORDING) {
+        port.postMessage({message: MESSAGE.START_RECORDING})
+    }
+    if (e.data === MESSAGE.STOP_RECORDING) {
+    }
+    if (e.data === MESSAGE.CHECK_STATUS) {
+        window.postMessage('extension-is-installed', window.location.origin)
+    }
+}
+
+port.onMessage.addListener(function(message) {
+    if (message.message === MESSAGE.STREAM_ID) {
+        var streamId = message.streamId
+        var opts = message.opts
+        console.log(streamId)
+        window.postMessage({streamId: streamId, opts: opts, message: MESSAGE.STREAM_ID}, window.location.origin)
+        // getStream(streamId, opts)
+    }
+    if (message.message === MESSAGE.USER_CANCELED) {
+        console.log('user-canceled')
+    }
+})
